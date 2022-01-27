fe175747a641bf5b7d1453146f06aa6dd3f78d85
shreyanish <shreyanish.g@gmail.com>
2022-01-27 17:27:43 +0530

    Update wsgi.py

--- unbranded/wsgi.py
+++ unbranded/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'knewy-test.unbranded.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'unbranded.settings')
 
 application = get_wsgi_application()
