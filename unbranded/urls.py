0cf50c874c114d23b6a0117bfab27a5fc1954d62
shreyanish <shreyanish.g@gmail.com>
2022-01-27 17:42:31 +0530

    commit

--- unbranded/urls.py
+++ unbranded/urls.py
@@ -18,11 +18,11 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.views.static import serve
-from django.conf.urls import url
+from django.conf.urls import urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    urls(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    urls(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
      
 ]
