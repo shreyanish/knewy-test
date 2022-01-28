2b5dd87c50cd41520a6232ebaf95187618858766
shreyanish <shreyanish.g@gmail.com>
2022-01-28 13:52:02 +0530

    Update settings.py

--- unbranded/settings.py
+++ unbranded/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-j8x5-ver=u(%b(!hswjkjw)%pso8kf%sut)zc3hbuq2lp$7u-_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
