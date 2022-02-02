646768f15b617a2e16125f7752a6ebdb7d9a7735
shreyanish <shreyanish.g@gmail.com>
2022-02-02 14:07:51 +0530

    Update settings.py

--- unbranded/settings.py
+++ unbranded/settings.py
@@ -131,6 +131,9 @@ STATICFILES_DIRS = [
 
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+WHITENOISE_USE_FINDERS = True
+WHITENOISE_MANIFEST_STRICT = False
+WHITENOISE_ALLOW_ALL_ORIGINS = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
